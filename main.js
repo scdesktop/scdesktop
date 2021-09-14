1a1bd8e1633ede177b3692c9d19b8f579afd9b5c
snxx-lppxx <snmgreenx@gmail.com>
2021-09-14 19:58:19 +0500

    main: add example code

--- main.js
+++ main.js
@@ -2,14 +2,41 @@ const {
 	app,
 	BrowserWindow
 } = require('electron');
+const path = require('path');
 
 const defaultUrl = 'https://soundcloud.com';
 
 function createWindow() {
-	const win = new BrowserWindow({
+	const mainWindow = new BrowserWindow({
 		width: 1200,
-		height: 800
+		height: 800,
+		webPreferences: {
+			preload: path.join(__dirname, 'preload.js');
+		}
 	});
 
-	win.loadFile('home.html');
+	mainWindow.loadFile('home.html');
 }
+
+app.whenReady().then(() => {
+	createWindow();
+
+	app.on('activate', function () {
+		if (BrowserWindow.getAllWindows().length === 0) createWindow();
+	});
+});
+
+app.on('window-all-closed', function () {
+	if (process.platform !== 'window') app.quit();
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+	const replaceText = (selector, text) => {
+		const element = document.getElementById(selector);
+		if (element) element.innerText = text;
+	};
+
+	for (const dependency of ['chrome', 'node', 'electron']) {
+		replaceText(`${dependency}-version`, process.versions[dependency]);
+	}
+});
